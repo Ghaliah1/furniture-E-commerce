8db6bca203b050a37a02a4e16378a2fd8e30d84b
ghaliah <aldossarighaliah@gmail.com>
2023-12-08 14:37:12 +0300

    change1

--- main.js
+++ main.js
@@ -9,17 +9,52 @@
  * product filter
  */
 
-const filterBtns = document.querySelectorAll("[data-filter-btn]");
-const filterBox = document.querySelector("[data-filter]");
+document.addEventListener('DOMContentLoaded', function () {
+  const filterButtons = document.querySelectorAll('.filter-btn');
+  const productItems = document.querySelectorAll('.product-list li');
 
-let lastClickedFilterBtn = filterBtns[0];
+  filterButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      const filter = this.getAttribute('data-filter-btn');
 
-const filter = function () {
-  lastClickedFilterBtn.classList.remove("active");
-  this.classList.add("active");
-  lastClickedFilterBtn = this;
+      productItems.forEach(function (item) {
+        if (filter === 'all' || item.classList.contains(filter)) {
+          item.style.display = 'block';
+        } else {
+          item.style.display = 'none';
+        }
+      });
 
-  filterBox.setAttribute("data-filter", this.dataset.filterBtn)
-}
+      // Show items that match the filter and hide others
+      productItems.forEach(function (item) {
+        if (filter === 'all' || item.classList.contains(filter)) {
+          item.style.display = 'block';
+        } else {
+          item.style.display = 'none';
+        }
+      });
 
-addEventOnElem(filterBtns, "click", filter);
+      // Add active class to the clicked button and remove it from others
+      filterButtons.forEach(function (btn) {
+        btn.classList.remove('active');
+      });
+      this.classList.add('active');
+    });
+  });
+});
+
+
+// const filterBtns = document.querySelectorAll("[data-filter-btn]");
+// const filterBox = document.querySelector("[data-filter]");
+
+// let lastClickedFilterBtn = filterBtns[0];
+
+// const filter = function () {
+//   lastClickedFilterBtn.classList.remove("active");
+//   this.classList.add("active");
+//   lastClickedFilterBtn = this;
+
+//   filterBox.setAttribute("data-filter", this.dataset.filterBtn)
+// }
+
+// addEventOnElem(filterBtns, "click", filter);
